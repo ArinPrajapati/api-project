b4dc26e7a8cac52603ec6eb06f5541a9d20122c9
Arin Prajapati <arinprajapati78@gmail.com>
2023-07-14 20:35:14 +0530

    the search is working and all but abhay did something like he shitfed the api-connection code in search say props was not passing porperly or whatever

--- src/component/Api_connect.jsx
+++ src/component/Api_connect.jsx
@@ -5,54 +5,15 @@ import Animal from "./Animal";
 
 export default function Api_connect(props) {
 
-  if (typeof props.name === "undefined") {
-    return null;
-  }
-
-  const [data, setData] = useState("");
-  const [loading, seLoading] = useState(false);
-  // const { name } = props.name || {};
   
 
- 
-
-  async function getData() {
-    try {
-      seLoading(true);
-      const response = await axios
-        .get(`https://api.api-ninjas.com/v1/animals?name=${props.name}`, {
-          headers: {
-            "X-Api-Key": "syADX+R4WkxmKMstuOE6Gg==ipIiD9ut7tcJ5X4w",
-          },
-        })
-        .then((response) => setData(response.data));
-    } catch (error) {
-      <h1>{error}</h1>
-    }
-
   
-  }
 
-  useEffect(() => {
-    getData();
-   
-  }, [props.name]);
-
-  seLoading(false);
-  
 
   return (
     <>
-      {loading == true ? <Loading /> : null}
-      {data && data.length > 0 ? (
-        <>{data.map((animal) => {
-         return <Animal animal={animal} />
-        })}</>
-      
-      ) : null}
-     
+    
 
-      {console.log(data)}
     </>
   );
  
--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,19 +1,49 @@
-import { useState } from "react"
-import Api_connect from "./Api_connect"
+import { useState, useEffect } from "react";
+import Animal from "./Animal";
+import axios from "axios";
+import Loading from "../Loading";
 
-export default function Search(){
-    const [item,setItem] = useState("");
-    const [paste , setPaste] = useState('')
-    return(
-       <>
-        <form action="">
-            <input type="text" value={item} onChange={(e) =>  setItem(e.target.value) } />
-            <button onClick={() => {
-                setPaste(item)
-            }}> click</button>
-          
-        </form>
-         <Api_connect name={paste}/>
-         </> 
-    )
+export default function Search() {
+  const [item, setItem] = useState("");
+  const [data, setData] = useState([]);
+  const [loading, seLoading] = useState(false);
+
+  async function getData(name) {
+    try {
+      seLoading(true);
+      const response = await axios
+        .get(`https://api.api-ninjas.com/v1/animals?name=${name}`, {
+          headers: {
+            "X-Api-Key": "syADX+R4WkxmKMstuOE6Gg==ipIiD9ut7tcJ5X4w",
+          },
+        })
+        .then((response) => setData(response.data));
+    } catch (error) {
+      <h1>{error}</h1>;
+    } finally {
+      seLoading(false);
+    }
+  }
+  return (
+    <>
+      <input
+        type="text"
+        
+        onChange={(e) => {
+         
+
+          getData(e.target.value);
+        }}
+      />
+
+      {loading == true ? <Loading /> : null}
+      {data && data.length > 0 ? (
+        <>
+          {data.map((animal) => {
+            return <Animal animal={animal} />;
+          })}
+        </>
+      ) : null}
+    </>
+  );
 }
